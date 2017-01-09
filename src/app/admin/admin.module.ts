fa7ed533934f81b39620d939b1dba8deb5a9b7ce
qpdian <qpdiam@gmail.com>
2017-01-08 20:21:28 -0500

    feat(map): add component for show map

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
-import { IndexComponent } from './index/index.component';
 import { RoutingModule } from './routing.module';
 import { ChartCustomModule } from '../shared/charts/charts.module';
-
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { IndexComponent } from './index/index.component';
+import { MapComponent } from './map/index.component';
 
 @NgModule({
   imports: [
-    CommonModule,MaterialModule.forRoot(),RoutingModule, ChartCustomModule
+    CommonModule,MaterialModule.forRoot(),RoutingModule, ChartCustomModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB8NAE6pJv8THMXrIBusSXIYesDDkQJILU'
+    })
   ],
-  declarations: [IndexComponent]
+  declarations: [IndexComponent,MapComponent]
 })
 export class AdminModule { }
