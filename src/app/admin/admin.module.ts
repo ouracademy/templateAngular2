b59277253910100878abd16382fc2664b429ea3e
Diana Quintanilla <qpdiam@gmail.com>
2017-01-13 02:26:08 -0500

    feat(map):integrate with firebase - todo refactorize all

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,6 +6,8 @@ import { ChartCustomModule } from '../shared/charts/charts.module';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { DndModule } from 'ng2-dnd';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
 import { IndexComponent } from './index/index.component';
 
 import { MapComponent } from './components/map/index.component';
@@ -20,17 +22,30 @@ import { GuideMapComponent } from './pageComponents/guideMap/index.component';
 import { DragAndDropComponent } from './pageComponents/dragDrop/index.component';
 import { OtherSamplesComponent } from './pageComponents/otherSamples/index.component';
 import { ReportChartsComponent } from './pageComponents/reportCharts/index.component';
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2VyvdslE3c-3As0ThAjcnVfhxjYwwy3o",
+  authDomain: "guidetravel-bd8a5.firebaseapp.com",
+  databaseURL: "https://guidetravel-bd8a5.firebaseio.com",
+  storageBucket: "guidetravel-bd8a5.appspot.com",
+  messagingSenderId: "410800102412"
+};
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
+
 @NgModule({
   imports: [
     CommonModule, MaterialModule.forRoot(), FormsModule, RoutingModule, ChartCustomModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB8NAE6pJv8THMXrIBusSXIYesDDkQJILU'
     }),
-    DndModule.forRoot()
+    DndModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig,myFirebaseAuthConfig)
   ],
   declarations: [IndexComponent, MapComponent, ShopCartComponent,
     AgileBoardComponent, ReaderFileComponent, ClockComponent,
-    SecuencyImageComponent,GeolocationComponent,
+    SecuencyImageComponent, GeolocationComponent,
     GuideMapComponent,
     DragAndDropComponent,
     OtherSamplesComponent,
--- src/app/admin/components/map/index.component.ts
+++ src/app/admin/components/map/index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-map',
@@ -7,33 +9,53 @@ import { Subscription, Observable } from 'rxjs';
   styleUrls: ['./index.component.css']
 })
 export class MapComponent implements OnInit {
-  title: string = 'Angular2-google-maps project';
+  title: string = '';
   lat: number = 0;
   lng: number = 0;
   loadLocation: boolean = false;
   zoom: number = 18;
   myPosition = { lat: 0, lng: 0 };
-  locations = [];
-  constructor() { }
+  locations: FirebaseListObservable<any[]>;
+  auth: any;
+  constructor(public af: AngularFire) {
+    this.locations = this.af.database.list('/locations');
+    this.af.auth.subscribe(auth => this.auth = auth);
+  }
+  addLocation(title, lat, lng, draggable = false) {
+    this.locations.push({ title: title, coords: { lat: lat, lng: lng }, draggable: draggable });
+  }
+  updateLocation(key: string, coords) {
+    this.locations.update(key, { coords: coords });
+  }
   ngOnInit() {
     this.showPosition();
   }
-  insertPosition(title, lat, lng, draggable = false) {
-    this.locations.push({ title: title, coords: { lat: lat, lng: lng }, draggable: draggable });
-  }
   addMarker() {
-    this.insertPosition('Change the position', this.lat + 0.0001, this.lng, true);
+    if (this.title !== '') {
+      this.addLocation(this.title, this.lat + 0.0001, this.lng, true);
+    }
   }
   showPosition() {
     navigator.geolocation.getCurrentPosition(
       (response) => {
         this.lat = this.myPosition.lat = response.coords.latitude;
         this.lng = this.myPosition.lng = response.coords.longitude;
-        this.insertPosition('My position', this.myPosition.lat, this.myPosition.lng);
         this.loadLocation = true;
       }, () => {
 
       });
+  }
+  login() {
+    this.af.auth.login();
+  }
+  logout() {
+    this.af.auth.logout();
+  }
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+  markerDragEnd(m, $event) {
+    this.updateLocation(m.$key, $event.coords);
   }
 
 }
